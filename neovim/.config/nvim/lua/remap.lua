b4d18bfd355d57c0cfca0fe3e3c8993ca065daae
David Weber <1501176+djweber@users.noreply.github.com>
2022-12-30 09:17:35 -0500

    add tree refresh keymap

--- neovim/.config/nvim/lua/remap.lua
+++ neovim/.config/nvim/lua/remap.lua
@@ -1,3 +1,4 @@
 vim.g.mapleader = " "
 vim.keymap.set("n", "<leader>nt", vim.cmd.NvimTreeToggle)
 vim.keymap.set("n", "<leader>nf", vim.cmd.NvimTreeFocus)
+vim.keymap.set("n", "<leader>nr", vim.cmd.NvimTreeRefresh)
