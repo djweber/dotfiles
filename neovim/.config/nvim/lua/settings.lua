10b694f46a24b2c3eb9faeb0e4711243f862c26b
David Weber <1501176+djweber@users.noreply.github.com>
2022-12-30 09:45:16 -0500

    update settings

--- neovim/.config/nvim/lua/settings.lua
+++ neovim/.config/nvim/lua/settings.lua
@@ -1,5 +1,24 @@
 HOME = os.getenv('HOME')
 
-vim.cmd('set number')
+vim.opt.guicursor = ""
+vim.opt.nu = true
+vim.opt.relativenumber = true
+vim.opt.tabstop = 4
+vim.opt.softtabstop = 4
+vim.opt.shiftwidth = 4
+vim.opt.expandtab = true
+
+vim.opt.smartindent = true
+vim.opt.wrap = false
+
+vim.opt.swapfile = false
+vim.opt.backup = false
+vim.opt.undodir = HOME .. "/.vim/undodir"
+vim.opt.undofile = true
+
+vim.opt.hlsearch = false
+vim.opt.incsearch = true
+
+vim.opt.termguicolors = true
+
 vim.opt.shortmess:append({ I = true })
-vim.api.nvim_set_keymap('n', '<leader>e', ':NvimTreeToggle<CR>', {noremap = true, silent = true})
